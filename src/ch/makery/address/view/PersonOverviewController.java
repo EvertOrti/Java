0eab0534bf9f337670d1fb44829e70a2d4abcb08
EvertOrti <evertorti@gmail.com>
2018-01-31 14:12:32 +0200

    Person edit controller

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TableView;
 import ch.makery.address.MainApp;
 import ch.makery.address.model.Person;
 import ch.makery.address.util.DateUtil;
+import javafx.scene.control.Alert.AlertType;
 
 public class PersonOverviewController {
     @FXML
@@ -102,6 +103,17 @@ public class PersonOverviewController {
     @FXML
     private void handleDeletePerson() {
         int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
-        personTable.getItems().remove(selectedIndex);
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Nothing selected.
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
     }
 }
