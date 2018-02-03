25a2e5384886946e6e65ceabcbbed85ecaf4d201
EvertOrti <evertorti@gmail.com>
2018-02-03 17:23:39 +0200

    Statistics menu handler

--- src/ch/makery/address/view/RootLayoutController.java
+++ src/ch/makery/address/view/RootLayoutController.java
@@ -115,4 +115,9 @@ public class RootLayoutController {
     private void handleExit() {
         System.exit(0);
     }
+
+    @FXML
+    private void handleShowBirthdayStatistics() {
+      mainApp.showBirthdayStatistics();
+    }
 }
